5fcf6184b7ee9c49fa4e1dc6b5540b405045ef05
OlivierGingras <ogingras013@gmail.com>
2024-11-06 11:28:42 -0500

    Enum

---
+++ Board.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Board
+    {
+        PlayerEnum[,] board = new PlayerEnum[3,3];
+
+        public Board()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = PlayerEnum.NONE;
+                }
+            }
+        }
+    }
+}
